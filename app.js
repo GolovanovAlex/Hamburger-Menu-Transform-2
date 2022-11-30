e0a35932c4c2ca8957dfa8fb321d77c081940e6e
GolovanovAlex <Golovanov.a.a@yandex.ru>
2022-11-30 05:34:04 +0300

    fin

--- app.js
+++ app.js
@@ -1,15 +1,5 @@
-// Click
-
 $('.burger').addClass('unToggled');
 $('.burger').click(function () {
   $(this).toggleClass('toggled');
   $(this).toggleClass('unToggled');
 });
-
-// Resizing Slider
-
-const inputs = document.querySelectorAll('input');
-const div = document.querySelector('.burger');
-function handleInputChange() {
-  const units = this.dataset.units || '';
-}
